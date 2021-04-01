b5f0691239690f7a6e54ba881d47822e9b734326
Diego Francisco <diegofrancisco.ks@gmail.com>
2021-04-01 16:53:19 +0200

    Adicionando mais funcionalidades...

--- tutorial/hello/hello.go
+++ tutorial/hello/hello.go
@@ -1,9 +1,39 @@
 package main
 
-import "fmt" // I/O funtions
-import "rsc.io/quote"
+import(
+	"fmt" // I/O funtions
+ 	"rsc.io/quote"
+	"example.com/greetings"
+	"log"
+)
  
 func main(){
-	//fmt.Println("Hello , World")
+	// Hello World	
+	fmt.Println("Hello , World")
+
+	// Importing and using a module
 	fmt.Println(quote.Go())
+
+	// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+    // Request a greeting message.
+    //message, err := greetings.Hello("Diego")
+	messages, err := greetings.Hellos(names)
+
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
+    fmt.Println(messages)
 }
